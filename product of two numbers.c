a3e113eb85fa059efd6d73b61e3ffd2fb7628e24
Josehmaina <123634501+Josehmaina@users.noreply.github.com>
2023-01-26 13:54:23 +0300

    Add files via upload

---
+++ product of two numbers.c
@@ -0,0 +1,13 @@
+/*ARITHMETIC OPERATION PRODUCT*/
+#include<stdio.h>
+int main()
+{
+	int a,b,product;
+	printf("\n ENTER VALUE OF A:");
+	scanf("%d",&a);
+	printf("\n ENTER THE VALUE OF B:");
+	scanf("%d",&b);
+	product=a*b;
+	printf("THE PRODUCT IS %d",product);
+	return 0;
+}
